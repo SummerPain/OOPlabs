35fb5d51bdf8f8ce535c19bbac98e415966e5e54
SummerPain <dmi.kozhuch@gmail.com>
2020-10-14 01:09:53 +0300

    New lab ver

---
+++ ooplab2/ooplab2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooplab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.a
+            bool type1 = true;
+            byte type2 = 7; //0-255
+            sbyte type3 = 13; //-127 - 127
+            char type4 = 'a';
+            decimal type5 = 3.535233576454m;
+            double type6 = 155.72;
+            float type7 = 4756.74183f;
+            int type8 = -124;    //-2147483648 - 2147483647
+            uint type9 = 349;   //0 - 4294967295
+            long type10 = -715208423597;
+            ulong type11 = 567356479;
+            short type12 = -11132;
+            ushort type13 = 5746;
+            object type14 = new object();
+            string type15 = "string type";
+            dynamic type16;
+
+            //1.b
+            //явное преоброзование 
+            double a0 = 2.3;
+            int b0;
+            b0 = (int)a0;
+            //неявное
+            int a1 = 4444;
+            long b1 = a1;
+
+            byte a2 = 255;
+            ushort b2 = a2;
+
+            uint a3 = 911;
+            ulong b3 = a3;
+
+            sbyte a4 = -127;
+            short b4 = a4;
+
+            //1.c
+            int topack = 10;
+            //
+            //упаковка
+            object pack = topack;
+            //распаковка
+            int tounpack = (int)pack;
+        }
+    }
+}
---
+++ ooplab2/ooplab2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ooplab2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ooplab2")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3da6d472-94f8-44a2-ac0b-e7ef0fd1ab35")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
