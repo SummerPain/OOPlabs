cfab7e969a85762cc4e1f9786e2f428d13006cc0
SummerPain <dmi.kozhuch@gmail.com>
2020-10-21 02:12:28 +0300

    added 3 lab

---
+++ ooplab3/lab3oop/lab3oop/Book.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3oop
+{
+    partial class Book
+    {
+        private static uint count = 0;
+        private uint id;
+        private string title;
+        private string author;
+        private string publisher;
+        private Int16 year;
+        private Int16 pages;
+        private float price;
+        private string binding;
+        //out example
+        public static void outexample(out int num) //работа с out
+        {
+            num = 88;
+        }
+        //info
+        public static void Info()
+        {
+            Console.WriteLine("\nИнформация объекта:\n");
+            Console.WriteLine("ID книги");
+            Console.WriteLine("Название книги");
+            Console.WriteLine("Имя автора");
+            Console.WriteLine("Год издания книги");
+            Console.WriteLine("Количество страниц");
+            Console.WriteLine("Цена книги");
+            Console.WriteLine("Тип переплёта\n\n");
+        }
+        //get/set funcs
+        public uint Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public uint ID
+        {
+            get
+            {
+                return id;
+            }
+            private set
+            {
+                while (value < 0)
+                {
+
+                    Console.WriteLine("ID был введён неверно, введите заново: ");
+                    value = Convert.ToUInt32(Console.ReadLine());
+                }
+                id = value;
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+
+                    throw new ArgumentNullException("Ошибка: пустое название");
+
+                }
+                title = value;
+            }
+        }
+        public string AuthorName
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+
+                    throw new ArgumentNullException("Ошибка: пустое имя автора");
+
+                }
+                author = value;
+            }
+        }
+
+        public string PublisherName
+        {
+            get
+            {
+                return publisher;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+
+                    throw new ArgumentNullException("Ошибка: пустое название издательства");
+
+                }
+                publisher = value;
+            }
+        }
+        public Int16 PublishingYear
+        {
+            get
+            {
+                return year;
+            }
+            private set
+            {
+                while (value <= 0 || value > 2020)
+                {
+                    Console.WriteLine("Год издания был введён некорректно, введите заново: ");
+                    value = Convert.ToInt16(Console.ReadLine());
+                }
+                year = value;
+            }
+        }
+        public Int16 PagesCount
+        {
+            get
+            {
+                return pages;
+            }
+            private set
+            {
+                while (value < 0)
+                {
+                    Console.WriteLine("Количество страниц было введено некорректно, введите заново: ");
+                    value = Convert.ToInt16(Console.ReadLine());
+                }
+                pages = value;
+            }
+        }
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+            private set
+            {
+                while (value <= 0 || value > 2020)
+                {
+                    Console.WriteLine("Цена была введена некорректно, введите заново: ");
+                    value = (float)Convert.ToDouble(Console.ReadLine());
+                }
+                price = value;
+            }
+        }
+        public string BindingType
+        {
+            get
+            {
+                return binding;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+
+                    throw new ArgumentNullException("Ошибка: пустое название типа переплёта");
+
+                }
+                binding = value;
+            }
+        }
+        //constructors
+
+        public Book()
+        {
+            count++;
+            this.title = "Default title";
+            this.author = "Default author";
+            this.publisher = "Default publisher";
+            this.year = 0;
+            this.pages = 0;
+            this.price = 0;
+            this.binding = "Default binding";
+            this.id = Hash(pages, (int)price, title);
+        }
+        public Book(string title, string author, string publisher, Int16 year, Int16 pages, float price, string binding)
+        {
+            count++;
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+            this.year = year;
+            this.pages = pages;
+            this.price = price;
+            this.binding = binding;
+            this.id = Hash(pages, (int)price, title);
+        }
+        static Book()
+        {
+            Console.WriteLine("/called static constructor/");
+        }
+    }
+    public partial class Book
+    {
+        public uint Hash(int pages, int price, string title)
+        {
+            return (uint)(count - pages*title.Length + price);
+        }
+    }
+    //переопред
+    public class NewClass : object
+    {
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ ooplab3/lab3oop/lab3oop/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3oop
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //аноним
+            Anonimous();
+            //out example
+            int testnum;
+            Book.outexample(out testnum);
+            Console.WriteLine("out example = " + testnum);
+            //objs
+            object[] books = new object[] {
+                new Book("b1", "a1", "p1", 2020, 400, 39.99f, "bb1"),
+                new Book("b2", "a2", "p2", 2019, 365, 34.99f, "bb2"),
+                new Book("b3", "a3", "p3", 2017, 312, 24.99f, "bb3"),
+                new Book("b4", "a2", "p4", 2016, 15, 20f, "bb4"),
+                new Book("b5", "a1", "p5", 2015, 2546, 100f, "bb5"),
+                new Book("b6", "a2", "p6", 2014, 123, 999f, "bb6"),
+                new Book("b7", "a4", "p7", 2013, 986, 49.99f, "bb7"),
+                new Book("b8", "a3", "p8", 2012, 111, 1f, "bb8"),
+            };
+            // список книг заданного автора;
+            Console.WriteLine("Поиск по автору, укажите автора: ");
+            string a_search = Console.ReadLine();
+            bool search_mark = false;
+            foreach(Book baut in books)
+            {
+                if(string.Compare(baut.AuthorName, a_search) == 0)
+                {
+                    WriteBookData(baut);
+                    search_mark = true;
+                }
+            }
+            if(!search_mark)
+            {
+                Console.WriteLine("По данному автору ничего не найдено.\n");
+            }
+            // список книг после определённого года издания
+            Console.WriteLine("Поиск по году издания, укажите год: ");
+            search_mark = false;
+            int y_search = Convert.ToInt16(Console.ReadLine());
+            foreach (Book byear in books)
+            {
+                if (byear.PublishingYear >= y_search)
+                {
+                    WriteBookData(byear);
+                    search_mark = true;
+                }
+            }
+            if (!search_mark)
+            {
+                Console.WriteLine("По данному году издания ничего не найдено.\n");
+            }
+            //переопред
+            Console.WriteLine("Результат переопределённой функции Equals: " + Convert.ToString(NewClass.Equals(books[0], books[1])));
+            Console.ReadKey();
+        }
+        static void Anonimous()
+        {
+            var anonimous = new { anstr = "sdtfgh", annum = 999};
+            Console.WriteLine($"Анонимный тип: {anonimous.GetType()}\n\n");
+        }
+        static void WriteBookData(Book book)
+        {
+            Console.WriteLine($"Книга:\n\tНазвание: {book.Title}\n\tАвтор: {book.AuthorName}\n\tИздательство: {book.PublisherName}\n\tГод издания: {book.PublishingYear}\n\tСтраниц: {book.PagesCount}\n\tЦена: {book.Price}$\n\tПереплёт: {book.BindingType}");
+        }
+    }
+}
